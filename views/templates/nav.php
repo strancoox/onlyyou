9f99a4139481e1517407c616e980968ca4a19c8c
Alex Stranc <aleksander_stranc@sluz.ch>
2024-12-19 17:14:39 +0100

    `Added Material Icons and updated styles and navigation`

--- views/templates/nav.php
+++ views/templates/nav.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
-        <li class="nav-left"><a href="home">Home &nbsp; &nbsp;</a></li>
-        <li class="nav-left"><a href="progress">Progress</a></li>
-        <li class="nav-left"><a href="workout">Workout</a></li>
+        <li class="nav-left"><a href="home">Home &nbsp; &nbsp;<span class="nav-icons material-icons">home</span></a></li>
+        <li class="nav-left"><a href="progress">Progress<span class="material-icons">signal_cellular_alt</span></a></li>
+        <li class="nav-left"><a href="workout">Workout<span class="material-icons">fitness_center</span></a></li>
     </ul>
 </nav>

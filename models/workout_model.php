29076e7c8780d12de86601ecca708c9e9a0d4abc
Alex Stranc <aleksander_stranc@sluz.ch>
2025-01-17 14:31:53 +0100

    updated design for wokrout view

--- models/workout_model.php
+++ models/workout_model.php
@@ -33,6 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (empty($errors)) {
         require 'core/plans.php';
         $plan = createTrainingplan($age, $weight, $goal, $days);
+        return $plan;
     }
         
 }
--- views/workout_view.php
+++ views/workout_view.php
@@ -3,21 +3,26 @@
 require "views/templates/nav.php";
 require "models/workout_model.php";
 
-var_dump($plan);
-
-if ($plan === null) {
-} else {
+if ($plan === null) 
+{
+} 
+else 
+{
 }
 ?>
 <div class="workout-layout">
     <div class="plan-container">
         <div class="plan-box">
-
+            <h2>Dein Plan:</h2>
+            
         </div>
     </div>
     <div class="tips-container">
         <div class="tips-box">
-            
+            <h2>Tipps:</h2>
         </div>
     </div>
 </div>
+<?php
+require "views/templates/footer.php";
+?>
